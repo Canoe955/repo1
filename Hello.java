462b393657aa7c839f3c1d32f070b613e5c96a94
canoe <canoe955yan@gmail.com>
2020-08-25 18:27:46 +0800

    在克隆端下载文件的这一端修改Hello.java并上传到远端仓库

--- Hello.java
+++ Hello.java
@@ -1,5 +1,7 @@
 public class Hello{
 	public static void main (String [] args ){
 		System.out.println("Hello world !!!");
+		//在另一端克隆端 把文件修改了 并上传到远端仓库
+		System.out.println("Hello world !!!  ****************");
 	}
 }
